bedce2bb2c478e7de23728414dd99fd7398f1517
FroakieSlayer <jonah.calvo@gmail.com>
2016-05-27 09:36:34 -0500

    Small update, adds a nice frame

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 public class Test {
     public static void main(String [] args){
         System.out.println("Hello World lol");
+        JFrame frame = new JFrame("FrameDemo");
+
+
     }
 }
