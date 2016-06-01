c355827a88d1afdb67a502f708eabba74aa091b6
Chris Minge <chrisminge@gmail.com>
2016-05-31 22:55:18 -0500

    CMinge committing over out

--- src/FirstWindow.java
+++ src/FirstWindow.java
@@ -2,24 +2,30 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
-public class FirstWindow extends JFrame {
+public class FirstWindow extends JFrame implements ActionListener {
     private static final long serialVersionUID = 1L;
     private boolean[][] isFilled;
+    private int delayForDrop;
     public FirstWindow(){
         super("xD");
         setSize(500,940);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         isFilled = new boolean[][] {{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},{false,false,false,false,false,false,false,false,false,false},};
         //this is the board and its 22,10 rows, columns. The first two rows are not displayed
+        delayForDrop=1000;
 
-        thehandler side = new thehandler();
+
     }
+
     public void paint(Graphics g){
         //note to self cords for top left square are 40 80 (41 81 for the non border part)
         g.fillRect(0,0,500,940);
         g.setColor(Color.WHITE);
+
         for(int i = 0; i<10;i++){
             for(int j=0; j<20;j++){
                 g.drawRect((i+1)*40,(j+2)*40,40,40);
@@ -61,7 +67,7 @@ public class FirstWindow extends JFrame {
                 }
 
                 try {
-                    Thread.sleep(300);                 //1000 milliseconds is one second.
+                    Thread.sleep(delayForDrop);                 //1000 milliseconds is one second.
                 } catch (InterruptedException ex) {
                     Thread.currentThread().interrupt();
                 }
@@ -70,5 +76,36 @@ public class FirstWindow extends JFrame {
             }
         }
     }
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        /*if (key == KeyEvent.VK_LEFT) {
+
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+
+        }
+
+        if (key == KeyEvent.VK_UP) {
+
+        }*/
+
+        if (key == KeyEvent.VK_DOWN) {
+            delayForDrop=100;
+        }
+    }
+    private class TAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            keyPressed(e);
+        }
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
 
 }
